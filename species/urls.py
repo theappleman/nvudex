ca7624c4e6e2d5c83fe50e4c1c233c8a18151007
Daniel Cordero <nvudex@xxoo.ws>
2018-12-19 12:26:06 +0000

    Allow dashes in species slugs

--- species/urls.py
+++ species/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 app_name = "species"
 urlpatterns = [
-    url(r'(?P<slug>\w+)/?$', views.species_detail, name="species_detail"),
+    url(r'(?P<slug>[-\w]+)/?$', views.species_detail, name="species_detail"),
 ]
