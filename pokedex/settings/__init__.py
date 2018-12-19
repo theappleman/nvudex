7f5854a3beb95a7f32241d22b3867726b8a39677
Daniel Cordero <nvudex@xxoo.ws>
2018-12-19 10:55:40 +0000

    Target ElementsConfig

--- pokedex/settings/__init__.py
+++ pokedex/settings/__init__.py
@@ -81,7 +81,7 @@ INSTALLED_APPS = [
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'djangae.contrib.security',
-        'elements',
+        'elements.apps.ElementsConfig',
 ]
 
 MIDDLEWARE = [
