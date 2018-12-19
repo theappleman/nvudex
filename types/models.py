785f2809c4dd684e92319add7d4372f6af6e5588
Daniel Cordero <nvudex@xxoo.ws>
2018-12-19 10:26:39 +0000

    Create Type model

--- types/models.py
+++ types/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    id = models.IntegerField(primary_key=True)
+    identifier = models.CharField(max_length=12, unique=True)
