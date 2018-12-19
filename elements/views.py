4bd85cb180a691fcb3aae004bb991753f85957aa
Daniel Cordero <nvudex@xxoo.ws>
2018-12-19 11:00:04 +0000

    Create ElementList view

--- elements/views.py
+++ elements/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Element
+
+class ElementList(ListView):
+    model = Element
+
+element_list = ElementList.as_view()
