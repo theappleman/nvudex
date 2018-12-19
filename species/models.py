a9c331f781f62e2855612fa6a6b4c84cac44e98a
Daniel Cordero <nvudex@xxoo.ws>
2018-12-19 11:25:39 +0000

    Add species model

--- species/models.py
+++ species/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Species(models.Model):
+    class Meta:
+        verbose_name_plural = 'species'
+
+    pokedex = models.IntegerField(primary_key=True)
+    identifier = models.CharField(max_length=20, unique=True)
+    types = models.ManyToManyField('elements.Element')
